9f670b43c8324fffb0a947f081129b79f4874c05
Mitchel Parish <mitchelparish@gmail.com>
2019-01-24 18:07:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,20 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    c = self.new
+    c.save
+    c
+  end 
+
+
+
+
+
+
+
+
+
   def self.all
     @@all
   end
@@ -12,6 +26,6 @@ class Song
 
   def find_or_create_by_name(name)
   end
-  
+
 
 end
